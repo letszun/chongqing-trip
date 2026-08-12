38f34bb9a77e7931b1e398bc60644df8010903e9
letszun <letszun@gmail.com>
2026-08-12 21:04:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cq-trip-v33-nav-alignment-fix';
+const CACHE='cq-trip-v34-full-today-plan';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
